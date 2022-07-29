e791c504e03f33e6292242184b917872f27cd7b2
Nuthelk <nuthelkcd@gmail.com>
2022-07-29 14:52:34 -0500

    login y styled components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Login from "../components/Login";
 
 function App() {
   return (
     <>
-      <BrowserRouter>{/* Aqui poner la app */}</BrowserRouter>
+      <BrowserRouter><Login /></BrowserRouter>
 
     </>
   );
