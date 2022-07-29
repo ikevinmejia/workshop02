1a68d1b15ed0b9bc505696bf5c181b6dd4fcb1ff
ikevinmejia <kevin_6958@hotmail.com>
2022-07-29 14:00:15 -0500

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <BrowserRouter>{/* Aqui poner la app */}</BrowserRouter>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
     </>
   );
 }
