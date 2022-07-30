960ee18969e6eb0e9bc71fcc1ab2c4f607b5490f
ikevinmejia <kevin_6958@hotmail.com>
2022-07-29 23:15:32 -0500

    ya estamos cansados

--- src/components/Comentario.jsx
+++ src/components/Comentario.jsx
@@ -1,40 +1,36 @@
-import React, { useEffect, useState } from "react";
-// import { Contexto } from "../context/Context";
+import React, { useEffect, useState, useContext } from "react";
+import { Contexto } from "../context/Context";
 import MapComment from "./MapComment";
 import axios from "axios";
 
 function Comentario({ descripcion }) {
-  // const { comentario } = useContext(Contexto);
-  // const { comentarios } = comentario;
-  const [zarnaHijueputa, setZarnaHijueputa] = useState("");
+  const { comentario: coment } = useContext(Contexto);
+  const { comentarios } = coment;
+  // const [zarnaHijueputa, setZarnaHijueputa] = useState("");
   const { id } = JSON.parse(localStorage.getItem("infoPost"));
 
-  const { data } = zarnaHijueputa;
+  // const { comentarios: dataAxios } = zarnaHijueputa.data;
 
-  // const { comentarios } = data;
+  // useEffect(() => {
+  //   const funcionSalvadora = async () => {
+  //     const initialNotes = await axios.get(
+  //       `https://data-sprint-02.herokuapp.com/post/${id}`
+  //     );
+  //     setZarnaHijueputa(initialNotes);
+  //   };
+  //   funcionSalvadora();
+  // }, []);
 
-  console.log(data);
-
-  useEffect(() => {
-    const funcionSalvadora = async () => {
-      const initialNotes = await axios.get(
-        `https://data-sprint-02.herokuapp.com/post/${id}`
-      );
-      setZarnaHijueputa(initialNotes);
-    };
-    funcionSalvadora();
-  }, []);
-
-  // console.log(comentarios);
+  // console.log(dataAxios);
 
   return (
     <div className="flex flex-col items-center justify-start">
       <p className="w-full max-w-4xl px-4 mt-4">{descripcion}</p>
-      {/* {comentarios != null
-        ? comentarios.map((e, i) => (
-            <MapComment key={i} usuario={"kevin"} comentario={e.comentario} />
+      {/* {dataAxios != null
+        ? dataAxios.map((e) => (
+            <MapComment key={e} usuario={"kevin"} comentario={e.comentario} />
           ))
-        : console.log("comentarios")} */}
+        : console.log(comentarios)} */}
     </div>
   );
 }
