32126b3b057d13e0b02ca6f715d7b9cd6386e0ec
Nuthelk <nuthelkcd@gmail.com>
2022-07-29 23:43:19 -0500

    comentarios arreglado

--- src/components/Comentario.jsx
+++ src/components/Comentario.jsx
@@ -2,41 +2,31 @@ import React, { useEffect, useState } from "react";
 // import { Contexto } from "../context/Context";
 import MapComment from "./MapComment";
 import axios from "axios";
+import { getData } from "../helpers/CRUD";
 
 function Comentario({ descripcion }) {
   // const { comentario } = useContext(Contexto);
   // const { comentarios } = comentario;
-  const [zarnaHijueputa, setZarnaHijueputa] = useState("");
-  const { id } = JSON.parse(localStorage.getItem("infoPost"));
+  const [zarnaHijueputa, setZarnaHijueputa] = useState(null);
+  const { id } = JSON.parse(localStorage.getItem("infoPost"))
+  const url = `https://data-sprint-02.herokuapp.com/post/${id}`
 
-  const { data } = zarnaHijueputa;
-
-  // const { comentarios } = data;
-
-  console.log(data);
 
   useEffect(() => {
-    const funcionSalvadora = async () => {
-      const initialNotes = await axios.get(
-        `https://data-sprint-02.herokuapp.com/post/${id}`
-      );
-      setZarnaHijueputa(initialNotes);
-    };
-    funcionSalvadora();
-  }, []);
-
-  // console.log(comentarios);
+    getData(setZarnaHijueputa, url)
+  }, [zarnaHijueputa]);
 
   return (
     <div className="flex flex-col items-center justify-start">
       <p className="w-full max-w-4xl px-4 mt-4">{descripcion}</p>
-      {/* {comentarios != null
-        ? comentarios.map((e, i) => (
-            <MapComment key={i} usuario={"kevin"} comentario={e.comentario} />
-          ))
-        : console.log("comentarios")} */}
+      {zarnaHijueputa != null
+        ? zarnaHijueputa.comentarios.map((e, i) => (
+          <MapComment key={i} usuario={"kevin"} comentario={e.comentario} />
+        ))
+        : console.log("comentarios")}
     </div>
   );
+
 }
 
 export default Comentario;
